5fa2677af5bc4055b2ca18b8cce4a691913f8492
Jhon Perez <jhonperez.j95@gmail.com>
2020-08-03 23:55:56 -0500

    notes

--- src/redux/types/types.js
+++ src/redux/types/types.js
@@ -1,9 +1,16 @@
 export const types = {
-  login: '[auth] login',
-  logout: '[auth] logout',
+  login: '[Auth] login',
+  logout: '[Auth] logout',
 
   uiSetError: '[UI] Set Error',
   uiRemoveError: '[UI] Remove Error',
   uiStartLoading: '[UI] Start loading',
   uiFinishLoading: '[UI] Finish loading',
+
+  notesAddNew: '[Notes] New Note',
+  notesActive: '[Notes] Active Note',
+  notesLoad: '[Notes] Load Notes',
+  notesUpdate: '[Notes] Updated Notes',
+  notesFileUrl: '[Notes] Updated Img Url',
+  notesDelete: '[Notes] Updated Img Url',
 };
